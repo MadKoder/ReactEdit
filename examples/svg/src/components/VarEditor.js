fb03a193acf65d742aa8e149ea34ec4e5173e4ed
MadKoder <pasqualato@hotmail.com>
2016-10-12 22:05:04 +0200

    Remove unused code in VarEditor

--- examples/svg/src/components/VarEditor.js
+++ examples/svg/src/components/VarEditor.js
@@ -27,21 +27,6 @@ export default class VarEditor extends Component {
     }
   }
 
-  handleValueChange(e) {
-    if(e.charCode == 13) {
-      const {name} = this.props;    
-      let expressionString = e.target.value;
-      let val = evalExpressionString(expressionString);
-      if(val !== null) {
-        this.props.setVarComputedExpression(name, expressionString);
-        this.props.setVarValue(name, val);
-      }
-      // We don't want enter key to be handled
-      e.preventDefault();
-      return false;
-    }
-  }
-
   handleExpressionFocus(e) {
     this.expressionFocused = true;
   }
@@ -50,30 +35,14 @@ export default class VarEditor extends Component {
     this.expressionFocused = false;
   }
 
-  handleValueFocus(e) {
-    this.valueFocused = true;
-  }
-
-  handleValueBlur(e) {
-    this.valueFocused = false;
-  }
-
   render() {
-    const {value, name, expression, computed} = this.props;
-    // if(expression === undefined) {
-    //   this.computedExpressionElement = null;
-    // } else if(!this.expressionFocused) {
-    //   this.computedExpressionElement = (
-    //     <textarea cols="30" rows="1" defaultValue={expression} onKeyPress={::this.handleExpressionChange} onFocus={::this.handleExpressionFocus}/>
-    //   )
-    // }
+    const {value, name, expression} = this.props;
     this.computedExpressionElement = (
       <textarea cols="30" rows="1" defaultValue={expression} onKeyPress={::this.handleExpressionChange} onFocus={::this.handleExpressionFocus} onBlur={::this.handleExpressionBlur}/>)
     return (
     <div className="hGroup">
       <textarea cols="10" rows="1" value={name} disabled/>
-      <textarea cols="30" rows="1" defaultValue={value} value={this.valueFocused ? undefined : value} onChange={::this.handleValueChange}  onKeyPress={::this.handleValueChange} 
-        disabled onFocus={::this.handleValueFocus} onBlur={::this.handleValueBlur}/>
+      <textarea cols="30" rows="1" value={value} disabled />
       {this.computedExpressionElement}
     </div>);
   }
