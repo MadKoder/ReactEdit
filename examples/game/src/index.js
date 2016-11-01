b8665bc8af846527638a7bb097880bd9f8759600
MadKoder <pasqualato@hotmail.com>
2016-11-01 22:50:48 +0100

    Experimenting with text

--- examples/game/src/index.js
+++ examples/game/src/index.js
@@ -8,23 +8,20 @@ import './style/main.css';
 
 const nbCellH = 16;
 const nbCellV = nbCellH;
-const cellWidth = 20;
+const cellWidth = 25;
 const cellHeight = cellWidth;
-const boardWidth = nbCellH * cellWidth;
+const leftWidth = 100;
+const boardWidth = nbCellH * cellWidth + leftWidth;
 const boardHeight = nbCellV * cellHeight;
 
 const cellStyle = observable({
-  stroke: '#608D08',
-  fill : '#BA0B0B'
+  stroke: '#BA0B0B',
+  fill : '#970808'
 });
 
 const hoveredCellStyle = observable(Object.assign({}, cellStyle, {
   fill : "#018D8D"
 }));
-
-const mainStyle = {
-  backgroundColor: '#808080'
-};
 
 let hoveredCellId = observable(-1);
 
@@ -55,22 +52,90 @@ const Cell = observer(({cellAttrib}) =>
   />
 );
 
+const manaMeterStyle = observable({
+  stroke: 'black',
+  strokeWidth: 0,
+  fill : '#3E0000'
+});
+
+const filledManaMeterStyle = observable(Object.assign({}, manaMeterStyle, {
+  // stroke: '#BA0B0B',
+  fill : "#92DB01"
+}));
+
+
+const manaMeterHeight = 100;
+const manaMeterWidth = 150;
+const maxMana = 100;
+
+const textStyle={
+  fill : "white"
+};
+
+const innerSvgStyle = {
+  overflow : "hidden",
+  fill : "black",
+  background : "black"
+}
+
+const textBoundingRectStyle = {
+  stroke : "black",
+  fill : "none"
+}
+
+const textBaseLine = 15;
+// const textBoundingSvgHeight = textBaseLine + 2;
+const textBoundingSvgHeight = textBaseLine + 42;
+const textBoundingRectHeight = textBoundingSvgHeight;
+
+const ManaMeter = observer(({mana}) => {
+  const filledHeight=manaMeterHeight * (mana / maxMana);
+  return (
+    <svg x={20} y={manaMeterHeight + 20}>
+      <rect 
+        width={manaMeterWidth} height={manaMeterHeight} style={manaMeterStyle}
+      />
+      <rect 
+        y={manaMeterHeight - filledHeight} width={manaMeterWidth} height={filledHeight} style={filledManaMeterStyle}
+      />
+      <svg y={manaMeterHeight + 20} width={manaMeterWidth} height={textBoundingSvgHeight} style={innerSvgStyle}>
+      <foreignObject x="0" y="40" width="100" height="20">
+        <body xmlns="http://www.w3.org/1999/xhtml">
+            <textarea cols="30" rows="1" defaultValue={mana}/>
+        </body>
+        </foreignObject>
+        <text 
+          fontFamily="Consolas"
+          fontSize="20" style={textStyle} x="1" y={textBaseLine}>
+          {mana}
+        </text>
+        <rect style={textBoundingRectStyle} width={manaMeterWidth} height={textBoundingRectHeight}/>
+      </svg>
+    </svg>
+  );
+});
+
 const Board = observer(({cellStyle}) => 
-  <svg width={boardWidth} height={boardHeight} viewBox={"0 0 " + boardWidth.toString() + " " + boardHeight.toString()}>
-    {
-      _.range(nbCellH * nbCellV).map(cellIndex => {
-        const y = Math.floor(cellIndex / nbCellH);
-        const x = cellIndex % nbCellH;
-        return (
-          <Cell key={cellIndex} cellAttrib={makeCellAttrib(x, y, cellStyle)}/>
-        )
-      })
-    }
+  <div>
+  <svg xmlns="http://www.w3.org/2000/svg"  y={0} width={boardWidth} height={boardHeight} viewBox={"0 0 " + boardWidth.toString() + " " + boardHeight.toString()}>
+    <ManaMeter mana={20} />
+    <svg x={manaMeterWidth + 50}>
+      {
+        _.range(nbCellH * nbCellV).map(cellIndex => {
+          const y = Math.floor(cellIndex / nbCellH);
+          const x = cellIndex % nbCellH;
+          return (
+            <Cell key={cellIndex} cellAttrib={makeCellAttrib(x, y, cellStyle)}/>
+          )
+        })
+      }
+    </svg>
   </svg>
+  </div>
 );
 
 render(
-  <Board style={mainStyle} cellStyle={cellStyle}>
+  <Board cellStyle={cellStyle}>
   </Board>,
   document.getElementById('root')
 );
